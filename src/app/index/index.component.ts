e6e7dc4b7e94699d53d6610d8d26ad576b73c82e
YaelNmr <yayithejoker90@hotmail.com>
2023-04-01 04:31:32 -0300

    update

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
+   //Acá se crean las variables de instancia para almacenar los datos con los que trata el servicio
 
+  constructor() { }     //dentro de los () del constructor se inyecta el Servicio 
+  ngOnInit(): void {
+    
+  }
 }
